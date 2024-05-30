21007323d966410c25abbe39785feb0f55163fe9
ChenYing Kuo <evshary@gmail.com>
2024-05-30 21:10:36 +0800

    Support warmup time.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- src/ping.cpp
+++ src/ping.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <algorithm>
+#include <atomic>
 #include "rclcpp/rclcpp.hpp"
 #include "simple_performance/msg/ping_pong.hpp"
 
@@ -31,7 +32,10 @@ class Ping : public rclcpp::Node
             message_->data.resize(this->payload_size_);
             std::fill(message_->data.begin(), message_->data.end(), 0);
 
-            // TODO: Support warmup
+            // Init 
+            this->start_time_ = high_resolution_clock::now();
+            this->total_expected_time_ = this->warmup_ + ((float)this->samples_ / (float)this->rate_);
+
             // TODO: Able to configure QoS (reliability, history, durability)
             rclcpp::QoS qos(rclcpp::KeepLast{16});
             ping_publisher_ = this->create_publisher<simple_performance::msg::PingPong>("ping", qos);
@@ -41,47 +45,57 @@ class Ping : public rclcpp::Node
         }
         
         bool is_stop() {
-            return this->result_.size() >= this->samples_;
+            time_point<high_resolution_clock> now = high_resolution_clock::now();
+            return (this->samples_idx_ >= this->samples_) && 
+                    (now - this->start_time_ > duration<double, std::milli>((this->total_expected_time_)  * 1000));
         }
   
     private:
         void timer_callback() {
             if (this->samples_idx_ < this->samples_) {
-                time_point<high_resolution_clock> start = high_resolution_clock::now();
+                time_point<high_resolution_clock> t1 = high_resolution_clock::now();
                 // Put the start time inside the message
-                uint8_t *ptr = (uint8_t *)&start;
-                for (unsigned long i = 0; i < sizeof(start); i++) {
+                uint8_t *ptr = (uint8_t *)&t1;
+                for (unsigned long i = 0; i < sizeof(t1); i++) {
                     message_->data[i] = *(ptr + i); 
                 }
                 ping_publisher_->publish(*message_);
-                this->samples_idx_++;
+                // Start to calculate after warmup
+                if (t1 - this->start_time_ > duration<double, std::milli>(this->warmup_ * 1000)) {
+                    this->samples_idx_++;
+                }
             }
         }
 
         void recv_pong_callback(const simple_performance::msg::PingPong::SharedPtr msg) {
-            time_point<high_resolution_clock> stop = high_resolution_clock::now();
+            time_point<high_resolution_clock> t2 = high_resolution_clock::now();
             // Retrieve the start time from the message
-            time_point<high_resolution_clock> start;
-            uint8_t *ptr = (uint8_t *)&start;
-            for (unsigned long i = 0; i < sizeof(start); i++) {
+            time_point<high_resolution_clock> t1;
+            uint8_t *ptr = (uint8_t *)&t1;
+            for (unsigned long i = 0; i < sizeof(t1); i++) {
                 *(ptr + i) = msg->data[i]; 
             }
             // Get RTT and RTT/2
-            auto rtt = duration<double, std::micro>(stop - start).count();
-            RCLCPP_INFO(this->get_logger(), "RTT: %lf(us), RTT/2: %lf(us)", rtt, rtt/2);
-            this->result_.push_back(rtt);
+            auto rtt = duration<double, std::micro>(t2 - t1).count();
+            // Start to record after warmup
+            if (t2 - this->start_time_ > duration<double, std::milli>(this->warmup_ * 1000)) {
+                RCLCPP_INFO(this->get_logger(), "RTT: %lf(us), RTT/2: %lf(us)", rtt, rtt/2);
+                this->result_.push_back(rtt);
+            }
         }
   
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Publisher<simple_performance::msg::PingPong>::SharedPtr ping_publisher_;
         rclcpp::Subscription<simple_performance::msg::PingPong>::SharedPtr pong_subscriber_;
         size_t samples_;
-        size_t samples_idx_;
+        std::atomic<std::size_t> samples_idx_;
         size_t payload_size_;
         float warmup_;
+        float total_expected_time_;
         int rate_;
         simple_performance::msg::PingPong::SharedPtr message_;
         std::vector<double> result_;
+        time_point<high_resolution_clock> start_time_;
 };
 
 int main(int argc, char *argv[]) {
